8e1ec84ce4936690026847591ceb22cf261ccd30
el-crag <azertyggmf@outlook.com>
2025-07-17 20:42:29 -0500

    Added styles or attributes to menu box

--- src/GraphicLcd.cpp
+++ src/GraphicLcd.cpp
@@ -82,6 +82,9 @@ void GraphicLCD::init() {
 
 void GraphicLCD::initMenu() {
 	this->menuBox = lv_obj_create(lv_screen_active());
+	lv_obj_set_style_radius(this->menuBox, 0, 0); // Sin esquinas redondeadas
+	lv_obj_set_style_shadow_width(this->menuBox, 0, 0); // Sin sombra
+	lv_obj_set_style_border_width(this->menuBox, 0, 0); // Sin borde
 	lv_obj_set_style_pad_all(this->menuBox, 0, 0);
 	lv_obj_set_size(this->menuBox, 20, DSP_VER_RES);  // 20px de ancho, 64px de alto
 	lv_obj_align(this->menuBox, LV_ALIGN_TOP_LEFT, 0, 0);  // pegado a la izquierda
